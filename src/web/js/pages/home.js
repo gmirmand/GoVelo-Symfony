075c61b9fcefcf2bd28f9efdc3872c08f42ced57
GaetanMSI <gmirmand@gmail.com>
2017-12-04 17:38:28 +0100

    improve gulpfile = Browsersync, js, css, +watch, view TWIGsetup

---
+++ src/web/js/pages/home.js
@@ -0,0 +1,13 @@
+var home = {
+    container: $('.page-home'), /*<- tu peux définir des selecteur*/
+    val: 'Coucou', /*<- ou des variables*/
+
+    init: function () {
+        console.log('Home init');
+        home.test();
+    },
+    test: function () {
+        home.container.append(home.val);
+        /* et utiliser les variables en haut comme ça */
+    }
+};
