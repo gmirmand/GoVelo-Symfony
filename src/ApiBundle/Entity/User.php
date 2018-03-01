2fa90a96ea0522c67ca62c337928cff84255f8ed
mboudjouhali <mlk_30@hotmail.fr>
2018-03-01 17:16:56 +0100

    Remove Medal From Announcements

--- src/ApiBundle/Entity/User.php
+++ src/ApiBundle/Entity/User.php
@@ -268,7 +268,7 @@ class User extends BaseUser {
     /**
      * @ORM\ManyToMany(targetEntity="ApiBundle\Entity\medal", inversedBy="user")
      * @ORM\JoinColumn(name="medal_id", referencedColumnName="id" ,nullable=true)
-     * @Groups({"getAnnouncement", "getUser"})
+     * @Groups({"getUser"})
      */
     private $medal;
 
