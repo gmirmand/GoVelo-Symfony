25afdf5a23bfb47b82b84a7df628868ef2314c1a
mboudjouhali <mlk_30@hotmail.fr>
2018-03-29 17:08:12 +0200

    Merge Landing into Master

--- src/ApiBundle/Controller/DefaultController.php
+++ src/ApiBundle/Controller/DefaultController.php
@@ -1,17 +1,59 @@
 <?php
 
-namespace ApiBundle\Controller;
+namespace AppBundle\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
 
 class DefaultController extends Controller
 {
     /**
-     * @Route("/")
+     * @Route("/", name="homepage")
      */
-    public function indexAction()
+    public function indexAction(Request $request)
     {
-        return $this->render('ApiBundle:Default:index.html.twig');
+        
+        return $this->render('default/index.html.twig');
+    }
+
+    /**
+     * @Route("/bonnes-pratiques", name="bonnes-pratiques")
+     */
+    public function bonnesPratiquesAction(Request $request)
+    {
+        return $this->render('pages/bonnes-pratiques.html.twig');
+    }
+
+    /**
+     * @Route("/cgu", name="cgu")
+     */
+    public function cguAction(Request $request)
+    {
+        return $this->render('pages/cgu.html.twig');
+    }
+
+    /**
+     * @Route("/assurance", name="assurance")
+     */
+    public function assuranceAction(Request $request)
+    {
+        return $this->render('pages/assurance.html.twig');
+    }
+
+    /**
+     * @Route("/guide-utilisation", name="guide-utilisation")
+     */
+    public function guideAction(Request $request)
+    {
+        return $this->render('pages/guide-utilisation.html.twig');
+    }
+
+    /**
+     * @Route("/mentions-legales", name="mentions-legales")
+     */
+    public function mentionsAction(Request $request)
+    {
+        return $this->render('pages/mentions-legales.html.twig');
     }
 }
